f599074281d1a4983a58d57170be96ad9d7a2ca1
Azka <azkahar@gmail.com>
2022-01-25 16:05:31 +0700

    ya begitulah perloginan admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function check(Request $request){
+        // validasi input 
+        $request->validate([
+            'email' => 'required|email|exists:admins,email',
+            'password' => 'required|min:5|max:30'
+        ],[
+            'email.exists' => 'This email is not exists'
+        ]);
+
+        $creds = $request->only('email', 'password');
+
+        if(Auth::guard('admin')->attempt($creds)){
+            return redirect()->route('admin.home');
+        } else {
+            return redirect()->route('admin.login')->with('fail', 'Wrong email or password');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::prefix('admin')->name('admin.')->group(function(){
 
     Route::middleware(['auth:admin'])->group(function(){
         Route::view('/home', 'dashboard.admin.home')->name('home');
+        Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
     });
     
 });
